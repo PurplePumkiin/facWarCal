0f6e16585999b9ea974ff83afc64a6c3400a6d3d
PurplePumkiin <scotyshot.business+git@gmail.com>
2023-06-12 19:14:04 -0400

    Gotta fix the directory structure

--- API/POST/generateKey.js
+++
@@ -1,5 +0,0 @@
-import crypto from 'crypto';
-
-// Generate a random encryption key with the required length
-const encryptionKey = crypto.randomBytes(16);
-console.log(encryptionKey.toString('hex'));
