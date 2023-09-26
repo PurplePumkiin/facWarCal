9065c3282e2e6300e243f5a5dc5e090e152e67ab
PurplePumkiin <scotyshot.business+git@gmail.com>
2023-09-26 16:38:23 -0400

    dwAwaW

---
+++ FRONTEND/login.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>WIP - Login</title>
+<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="header"></div>
+        <div id="content">
+            <container id="loginBox">
+                <form>
+                    <input type="text" placeholder="username">
+                    <input type="password" placeholder="password">
+                    <a>Login</a>
+                </form>
+            </container>
+        </div>
+    </div>
+</body>
+</html>
